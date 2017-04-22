c17fa364ce7c013a2b580cd40a9a8e3b1a9ecb8e
Pzk1988 <knapik.przemyslaw@gmail.com>
2017-04-22 08:08:23 +0200

    first commit

---
+++ SportStore/SportStore.Domain/Repository/Database/DatabaseRepository.cs
@@ -0,0 +1,25 @@
+﻿using SportsStore.Domain.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SportsStore.Domain.Entities;
+using System.Data.Entity;
+
+namespace SportStore.Domain.Repository.Database
+{
+    public class DatabaseRepository : IProductRepository
+    {
+        public IEnumerable<Product> Products
+        {
+            get
+            {
+                using (CS_DatabaseEntities context = new CS_DatabaseEntities())
+                {
+                    return context.Products.Select(p => new Product { Category = p.Category, Description = p.Description, Name = p.Name, Price = p.Price, ProductID = p.ProductID }).ToList();
+                }
+             } 
+        }
+    }
+}
---
+++ SportStore/SportStore.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SportStore.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Bombardier Transportation (ZWUS) Polska Sp.z o.o.")]
+[assembly: AssemblyProduct("SportStore.UnitTests")]
+[assembly: AssemblyCopyright("Copyright © Bombardier Transportation (ZWUS) Polska Sp.z o.o. 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f96a19f4-2bae-418d-b52c-848cfe948243")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
